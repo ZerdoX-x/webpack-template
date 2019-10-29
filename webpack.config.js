ac2c9d7771132746de0fc14c5bd94ab12bef2694
ZerdoX-x <zerdox.cool4@gmail.com>
2019-10-29 12:42:59 +0500

    fonts added

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,18 @@ module.exports = {
             },
           },
         ],
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              context: 'src/assets/fonts',
+              outputPath: 'fonts',
+            },
+          },
+        ],
       },
     ],
   },
